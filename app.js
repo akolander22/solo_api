7e89d3036fb122c697fb152116ee8099d067663e
Andrew Kolander <akolander22@gmail.com>
2016-06-30 19:40:46 -0500

    initial commit

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ $.get('http://www.omdbapi.com/?t=goodfellas&y=&plot=short&r=json')
     var year = data.Year;
     var runTime = data.Runtime;
     var poster = data.Poster;
-    $('.movieOne').append('<img src="' + poster + '">')
-    $('.movieOne').append('<p class="one">'+ name +'</p>');
-    $('.one').append('<p>Released in: ' + year + ' ' + runTime + '</p>')
-  });
-$.get('http://www.omdbapi.com/?t=anchorman&y=&plot=short&r=json')
+    $('.movieOne').append('<img src="' + poster + '">',
+    '<p class="one">'+ name +'</p>',
+    '<p>Released in: ' + year + '</p>',
+    '<p>' + runTime + '</p>' );
+    });
+$.get('http://www.omdbapi.com/?t=anchorman%3A+the+legend+of+ron+burgundy&y=&plot=short&r=json')
   .then(function(data){
     console.log(data);
 
@@ -19,9 +20,9 @@ $.get('http://www.omdbapi.com/?t=anchorman&y=&plot=short&r=json')
     var year =data.Year;
     var runTime = data.Runtime;
     var poster = data.Poster;
-    $('.movieTwo').append('<img src="">')
-    $('.movieTwo').append('<p class="two">'+ name +'</p>');
-    $('.two').append('<p>Released in: ' + year + ' ' + runTime + '</p>')
+    $('.movieTwo').append('<img src="' + poster + '">',
+    '<p class="two">'+ name +'</p>','<p>Released in: ' + year + '</p>',
+    '<p>' + runTime + '</p>');
   })
   $.get('http://www.omdbapi.com/?t=12+angry+men&y=&plot=short&r=json')
   .then(function(data){
@@ -31,8 +32,7 @@ $.get('http://www.omdbapi.com/?t=anchorman&y=&plot=short&r=json')
     var year =data.Year;
     var runTime = data.Runtime;
     var poster = data.Poster;
-    $('.movieThree').append('<img src="' + poster + '">')
-    $('.movieThree').append('<p class="three">'+ name +'</p>');
-    $('.three').append('<p>Released in : ' + year + ' ' + runTime + '</p>')
+    $('.movieThree').append('<img src="' + poster + '">','<p class="three">'+ name +'</p>',
+    '<p>Released in : ' + year + '</p>','<p>' + runTime + '</p>');
   })
 });
